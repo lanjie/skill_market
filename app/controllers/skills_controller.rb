d028dd901f148914777b7f78ac908d47b24572e6
Jie Lan <j.lan@ncl.ac.uk>
2015-03-18 14:21:25 +0800

    Add worker and skill

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,31 @@
+class SkillsController < ApplicationController
+
+	before_action :set_worker
+	
+	def create		
+		@skill = @worker.skills.create(skill_param)
+
+		redirect_to @worker
+	end
+
+	def destroy
+		@skill = @worker.skills.find(params[:id])
+		if @skill.destroy
+			flash[:success] = "Skill has been deleted."
+		else
+			flash[:error] = "Skill cannot be deleted."
+		end
+
+		redirect_to @worker
+	end
+
+	private
+
+	def set_worker
+		@worker = Worker.find(params[:worker_id])
+	end
+
+	def skill_param
+		params[:skill].permit(:name)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :workers do 
+    resources :skills
+  end
+
+  root "workers#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
